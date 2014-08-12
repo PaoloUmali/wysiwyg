8f1faeea39ddbf419d27f31f1acc32a1de631135
Paolo Umali <dev@paoloumali.com>
2014-08-12 12:27:09 +0800

    start paoloumali/wysiwyg

---
+++ src/Paoloumali/Wysiwyg/WysiwygServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Paoloumali\Wysiwyg;
+
+use Illuminate\Support\ServiceProvider;
+
+class WysiwygServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('paoloumali/wysiwyg');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
